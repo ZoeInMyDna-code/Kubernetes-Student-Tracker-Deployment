3e6fb6de0382e7c946bddaba0146057f885d6d28
Ubuntu <ubuntu@ip-10-0-0-8.ec2.internal>
2025-08-07 16:44:25 +0000

    End of Project Push

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#Update System
+#!/bin/bash
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y curl apt-transport-https ca-certificates gnupg lsb-release
+
+
+ #Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -Ls https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+
+# Install kind
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/latest/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+
